420a26f58c6425ffe9b7d3be5d69e7237e5599ed
NelsonAmaya19 <nelsoneduardoac@ufps.edu.co>
2020-05-07 07:31:06 -0500

    Cuarto Commit

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,3 +1,21 @@
 $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 });
+
+ $(document).ready(function(){
+        $('#contactar').click(function(){
+         if($('#nombre').val()==""){
+          document.getElementById("nombre").focus();
+                alert("Debes Ingresar tu Usuario ");               
+                return false;
+            }else if($('#mail').val()==""){
+              document.getElementById("mail").focus();
+                alert("Debes Ingresar Email");               
+                return false;
+            }else if($('#campo').val()==""){
+              document.getElementById("campo").focus();
+                alert("Debes Ingresar un Mensaje");               
+                return false;
+            }
+});
+    });
